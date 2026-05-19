dda1026df7ca73122281fb71c74c07887ac4a678
Ramba Ganesh <ammananna8513@gmail.com>
2026-05-19 13:21:48 +0530

    Completed backend setup

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, getTasks
 
 urlpatterns = [
     path('', home),
+    path('tasks/', getTasks),
 ]
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,8 +1,15 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Task
+from .serializers import TaskSerializer
 
 @api_view(['GET'])
 def home(request):
-    return Response({
-        "message": "Task Manager Backend Working"
-    })
+    return Response({"message": "Backend Working"})
+
+
+@api_view(['GET'])
+def getTasks(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
